e9dbecc323c9d08a2a4bcdf69025afbbf5243329
Miguel Lino <contatomiguellino@gmail.com>
2021-10-03 23:10:35 -0300

    first add

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="$emit('btn-click')" :style="{background: color}" class="btn">{{ text }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    emits: ['btn-click']
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'darkred' : 'darkgreen'" @btn-click="$emit('form-toggle')" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        Button
+    },
+    emits: ['form-toggle']
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
